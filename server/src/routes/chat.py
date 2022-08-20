741dd4169a3f088cc4257efed1646147bca5041b
Nandakishor V <80710226+NandakishorV@users.noreply.github.com>
2022-08-20 22:50:10 +0530

    stored rejson in redis

--- server/src/routes/chat.py
+++ server/src/routes/chat.py
@@ -1,10 +1,12 @@
 import os
 from fastapi import APIRouter, FastAPI, WebSocket,  Request,Depends
 import uuid
+from rejson import Path
 from ..socket.connection import ConnectionManager
 from ..socket.utils import get_token
 from ..redis.producer import Producer
 from ..redis.config import Redis
+from ..schema.chat import Chat
 
 manager = ConnectionManager()
 chat = APIRouter()
@@ -16,16 +18,30 @@ redis = Redis()
 
 @chat.post("/token")
 async def token_generator(name: str, request: Request):
+    token = str(uuid.uuid4())
 
     if name == "":
         raise HTTPException(status_code=400, detail={
             "loc": "name",  "msg": "Enter a valid name"})
 
-    token = str(uuid.uuid4())
+    # Create new chat session
+    json_client = redis.create_rejson_connection()
 
-    data = {"name": name, "token": token}
+    chat_session = Chat(
+        token=token,
+        messages=[],
+        name=name
+    )
 
-    return data
+    # Store chat session in redis JSON with the token as key
+    json_client.jsonset(str(token), Path.rootPath(), chat_session.dict())
+
+    # Set a timeout for redis data
+    redis_client = await redis.create_connection()
+    await redis_client.expire(str(token), 3600)
+
+
+    return chat_session.dict()
 
 
 # @route   POST /refresh_token
